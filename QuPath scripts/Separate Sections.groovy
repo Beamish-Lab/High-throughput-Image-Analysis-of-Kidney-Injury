a897e2558b992b032ab1fcd8ff139988344da877
madimce <120478260+madimce@users.noreply.github.com>
2023-04-19 11:48:00 -0400

    Update Separate Sections.groovy

--- QuPath scripts/Separate Sections.groovy
+++ QuPath scripts/Separate Sections.groovy
@@ -1,16 +1,16 @@
 // Extract individual sections from a larger qpTIFF image and stores them as a pyramidal tif file
 // All regions of the images must be annotated AND classified (using a unique key such as "Row1_Column1" etc)
-// To complete this operation for all the images in the project, use in conjunction with "Run ‣ Run for project" 
+// To complete this operation for all the images in the project, use in conjunction with "Run ‣ Run for project"
 // In this macro "Annotations" generated in QuPath are used as ROIs to extract
 
-//12-9-2022 JAB
+//4-19-23 JFS
 
 //import the default methods (may be redundant)
 import static qupath.lib.gui.scripting.QPEx.*
 
 //import additional special methods for this evaluation  (may be redundant)
-import qupath.lib.images.writers.ome.OMEPyramidWriter 
-import qupath.lib.regions.ImageRegion 
+import qupath.lib.images.writers.ome.OMEPyramidWriter
+import qupath.lib.regions.ImageRegion
 
 //get details from the project
 def project = getProject()
@@ -19,7 +19,7 @@ def server = getCurrentServer()
 //get image name and remove the extra fluff on the end
 def imageData = getCurrentImageData()
 def name = imageData.getServer().getMetadata().getName() //get the full file name of the entry
-def shortname = name.split('_')[0] //take only the first part of the name before the '_' that is added when 
+def shortname = name.split('_')[0] //take only the first part of the name before the '_' that is added when
 
 //Make sure the location you want to save the files to exists - requires a Project
 def pathOutput = buildFilePath(PROJECT_BASE_DIR, 'Extracted Multichannel Whole Sections')
@@ -32,7 +32,7 @@ def classifiedAnnotations = getAnnotationObjects().findAll{it.getPathClass() != 
 classifiedAnnotations.each{anno ->  //this is groovy code for a loop that passes the annotation object into the loop for each
 
     //Name of the file and the path to where it goes in the Project
-    def fileName = pathOutput+"\\"+shortname+'_'+ anno.getPathClass().getName()+'.tif'
+    def fileName = pathOutput+"\\"+shortname+'_'+ anno.getPathClass().getName()+'.ome.tif'
 
     //For each annotation, we get its outline
     def roi = anno.getROI()
@@ -47,7 +47,7 @@ classifiedAnnotations.each{anno ->  //this is groovy code for a loop that passes
         .scaledDownsampling(1, 4) //generates an image with 4 levels of downsampling
         .region(requestROI)//this needs to come BEFORE "build()"
         .build()
-        .writePyramid(fileName)    
+        .writeSeries(fileName)    
 }
 
 print "DONE"
